0a46c23074b51fe6750827a1dbf39e7fc3931206
Dustin Younse <milsyobtaf@gmail.com>
2013-11-01 23:12:02 -0500

    initial commit

---
+++ page-front.tpl.php
@@ -0,0 +1,141 @@
+<?php ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
+<head>
+<?php print $head ?>
+  <title><?php print $head_title ?></title>
+  <?php print $styles ?>
+  <?php print $scripts ?>
+  
+  <meta name="description" content="" />
+  <meta name="keywords" content="" />
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js"></script>
+  <script type="text/javascript">
+    WebFont.load({
+      google: {
+      families: ['Minion']
+      }
+    });
+  </script>
+
+</head>
+<body>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+            <!--layout-->
+
+    <div class="HeaderFrame">
+    <div class="Header">
+      <div class="Logo">
+        <a href="http://www.parnassusbooks.net" title="Parnassus Books"><img src="/sites/parnassus.indiebound.com/themes/parnassus/Images/logo.png" alt="Parnassus Books" /></a>
+      </div>
+      <div class="TagLine">
+        <h4>An Independent Bookstore<br />For Independent People</h4>
+      </div>
+      <div class="LoginTab">
+        <a href="/user" title="Login" class="Login">Login</a>
+        <a href="/cart" title="Shopping Cart" class="Cart">Shopping Cart</a>
+      </div>
+      <div class="QuickSearch">
+        <h3>Find a Book</h3>
+        <?php print $search_box ?>
+      </div>
+    </div>
+  </div>
+  <div class="SiteFrame">
+    <div class="Nav">
+      <div class="TopLine"></div>
+      <div class="Social">
+<div class="fb-like" data-href="https://www.facebook.com/parnassusbooks1" data-send="false" data-layout="button_count" data-width="100" data-show-faces="false"></div>
+<div class="twitter"><a href="https://twitter.com/#!/ParnassusBooks1"><img src="/sites/parnassus.indiebound.com/themes/parnassus/Images/twit-icon.png" alt="Twitter" /></a></div>
+      </div>
+      <div class="Links">
+        <?php echo theme('links', $primary_links, array('class' => 'links primary-links menu-subcategories')) 
+        
+        ?>
+        
+      </div>
+      <div class="BtmLine"></div>
+    </div>
+    
+            <!-- /header -->
+    
+    <div class="Content">
+
+      <?php if ($left): ?>
+      <div class="LeftColFront">
+<?php print $left ?>
+      </div>
+      <?php endif; ?>
+     
+            <!--/left-column-->
+        
+      <div class="MidColFront">
+        <?php print $breadcrumb; ?>
+        <div class="Clear20"></div>
+          <?php if ($tabs): print '<div id="tabs-wrapper" class="clear-block">'; endif; ?>
+          <?php if ($node) { if ($node->type !='ababook' && $node->type != 'gbook') { ?>
+          <?php if ($title): print '<h2'. ($tabs ? ' class="with-tabs"' : '') .'>'. $title .'</h2>'; endif; ?>
+          <?php }} else { ?>
+          <?php if ($title): print '<h2'. ($tabs ? ' class="with-tabs"' : '') .'>'. $title .'</h2>'; endif; ?>
+          <?php } ?>
+          <?php if ($tabs): print '<ul class="tabs primary">'. $tabs .'</ul></div>'; endif; ?>
+          <?php if ($tabs2): print '<ul class="tabs secondary">'. $tabs2 .'</ul>'; endif; ?>
+          <?php if ($show_messages && $messages): print $messages; endif; ?>
+        
+        <div class="MidColContent">
+              <?php print $content ?>
+        </div>
+              <?php print $feed_icons; ?>
+      </div>
+            
+            <!--/content-->
+      
+      <?php if ($right): ?>
+      <div class="RightColFront">
+      <?php print $right ?>
+      </div>
+      <?php endif; ?>
+            
+            <!--/right-column-->
+  
+  
+  <div class="FooterFrame">
+    <div class="Divider"></div>
+    <div class="Footer">
+      <div class="Social">
+<div class="fb-like" data-href="https://www.facebook.com/parnassusbooks1" data-send="false" data-layout="button_count" data-width="100" data-show-faces="false"></div>
+<div class="twitter"><a href="https://twitter.com/#!/ParnassusBooks1"><img src="/sites/parnassus.indiebound.com/themes/parnassus/Images/twit-icon.png" alt="Twitter" /></a></div>
+      </div>
+      <p class="Location">
+        Store Location: 3900 Hillsboro Pike, Nashville, TN &nbsp;:&nbsp; 615.953.2243 &nbsp;:&nbsp; Open Mon-Sat 10am-8pm. Sun 12pm-5pm
+      </p>
+      <p>
+        All content © 2011 by Parnassus Books unless otherwise noted.
+      </p>
+      <p>
+        Website powered by the American Booksellers Association and IndieCommerce. 
+        Design by <a href="http://www.familytreedesign.net" title="Familytree">Familytree</a>.
+      </p>
+      <div class="Clear20"</div>
+    </div>
+  </div>
+  
+            <!--/footer-->
+  
+            <!--/layout-->
+  
+
+  <?php print $closure ?>
+  </body>
+</html>
---
+++ template.php
@@ -0,0 +1,194 @@
+<?php
+
+/**
+ * Sets the body-tag class attribute.
+ *
+ * Adds 'sidebar-left', 'sidebar-right' or 'sidebars' classes as needed.
+ */
+function phptemplate_body_class($left, $right) {
+  if ($left != '' && $right != '') {
+    $class = 'sidebars';
+  }
+  else {
+    if ($left != '') {
+      $class = 'sidebar-left';
+    }
+    if ($right != '') {
+      $class = 'sidebar-right';
+    }
+  }
+
+  if (isset($class)) {
+    print ' class="'. $class .'"';
+  }
+}
+
+/**
+ * Return a themed breadcrumb trail.
+ *
+ * @param $breadcrumb
+ *   An array containing the breadcrumb links.
+ * @return a string containing the breadcrumb output.
+ */
+function parnassus_breadcrumb($breadcrumb) {
+  if (!empty($breadcrumb)) {
+    return '<div class="CrumbNav">'. implode(' › ', $breadcrumb) .'</div>';
+  }
+}
+
+/**
+ * Override or insert PHPTemplate variables into the templates.
+ */
+function phptemplate_preprocess_page(&$vars) {
+  $vars['tabs2'] = menu_secondary_local_tasks();
+
+  // Hook into color.module
+  if (module_exists('color')) {
+    _color_page_alter($vars);
+  }
+}
+
+/**
+ * Add a "Comments" heading above comments except on forum pages.
+ */
+function parnassus_preprocess_comment_wrapper(&$vars) {
+  if ($vars['content'] && $vars['node']->type != 'forum') {
+    $vars['content'] = '<h2 class="comments">'. t('Comments') .'</h2>'.  $vars['content'];
+  }
+}
+
+/**
+ * Returns the rendered local tasks. The default implementation renders
+ * them as tabs. Overridden to split the secondary tasks.
+ *
+ * @ingroup themeable
+ */
+function phptemplate_menu_local_tasks() {
+  return menu_primary_local_tasks();
+}
+
+/**
+ * Returns the themed submitted-by string for the comment.
+ */
+function phptemplate_comment_submitted($comment) {
+  return t('!datetime — !username',
+    array(
+      '!username' => theme('username', $comment),
+      '!datetime' => format_date($comment->timestamp)
+    ));
+}
+
+/**
+ * Returns the themed submitted-by string for the node.
+ */
+function phptemplate_node_submitted($node) {
+  return t('!datetime — !username',
+    array(
+      '!username' => theme('username', $node),
+      '!datetime' => format_date($node->created),
+    ));
+}
+
+/**
+ * Generates IE CSS links for LTR and RTL languages.
+ */
+function phptemplate_get_ie_styles() {
+  global $language;
+
+  $iecss = '<link type="text/css" rel="stylesheet" media="all" href="'. base_path() . path_to_theme() .'/fix-ie.css" />';
+  if ($language->direction == LANGUAGE_RTL) {
+    $iecss .= '<style type="text/css" media="all">@import "'. base_path() . path_to_theme() .'/fix-ie-rtl.css";</style>';
+  }
+
+  return $iecss;
+}
+
+function parnassus_pager($tags = array(), $limit = 10, $element = 0, $parameters = array(), $quantity = 5) {
+  global $pager_page_array, $pager_total;
+
+  // Calculate various markers within this pager piece:
+  // Middle is used to "center" pages around the current page.
+  $pager_middle = ceil($quantity / 2);
+  // current is the page we are currently paged to
+  $pager_current = $pager_page_array[$element] + 1;
+  // first is the first page listed by this pager piece (re quantity)
+  $pager_first = $pager_current - $pager_middle + 1;
+  // last is the last page listed by this pager piece (re quantity)
+  $pager_last = $pager_current + $quantity - $pager_middle;
+  // max is the maximum page number
+  $pager_max = $pager_total[$element];
+  // End of marker calculations.
+
+  // Prepare for generation loop.
+  $i = $pager_first;
+  if ($pager_last > $pager_max) {
+    // Adjust "center" if at end of query.
+    $i = $i + ($pager_max - $pager_last);
+    $pager_last = $pager_max;
+  }
+  if ($i <= 0) {
+    // Adjust "center" if at start of query.
+    $pager_last = $pager_last + (1 - $i);
+    $i = 1;
+  }
+  // End of generation loop preparation.
+
+  $li_first = theme('pager_first', (isset($tags[0]) ? $tags[0] : t('«')), $limit, $element, $parameters);
+  $li_previous = theme('pager_previous', (isset($tags[1]) ? $tags[1] : t('‹')), $limit, $element, 1, $parameters);
+  $li_next = theme('pager_next', (isset($tags[3]) ? $tags[3] : t('›')), $limit, $element, 1, $parameters);
+  $li_last = theme('pager_last', (isset($tags[4]) ? $tags[4] : t('»')), $limit, $element, $parameters);
+
+  if ($pager_total[$element] > 1) {
+    if ($li_first) {
+      $items[] = array(
+        'class' => 'pager-first', 
+        'data' => $li_first,
+      );
+    }
+    if ($li_previous) {
+      $items[] = array(
+        'class' => 'pager-previous', 
+        'data' => $li_previous,
+      );
+    }
+
+    // When there is more than one page, create the pager list.
+    if ($i != $pager_max) {
+      // Now generate the actual pager piece.
+      for (; $i <= $pager_last && $i <= $pager_max; $i++) {
+        if ($i < $pager_current) {
+          $items[] = array(
+            'class' => 'pager-item', 
+            'data' => theme('pager_previous', $i, $limit, $element, ($pager_current - $i), $parameters),
+          );
+        }
+        if ($i == $pager_current) {
+          $items[] = array(
+            'class' => 'pager-current', 
+            'data' => $i,
+          );
+        }
+        if ($i > $pager_current) {
+          $items[] = array(
+            'class' => 'pager-item', 
+            'data' => theme('pager_next', $i, $limit, $element, ($i - $pager_current), $parameters),
+          );
+        }
+      }
+    }
+    // End generation.
+    if ($li_next) {
+      $items[] = array(
+        'class' => 'pager-next', 
+        'data' => $li_next,
+      );
+    }
+    if ($li_last) {
+      $items[] = array(
+        'class' => 'pager-last', 
+        'data' => $li_last,
+      );
+    }
+    return theme('item_list', $items, NULL, 'ul', array('class' => 'pager'));
+  }
+}
